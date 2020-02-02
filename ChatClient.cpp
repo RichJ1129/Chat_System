b63b6fa6b7c2392b3f6506de68ac18c99f9c78df
Richard Joseph <richjoseph1129@gmail.com>
2020-02-02 00:43:31 -0500

    Initial

---
+++ ChatClient.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Richard Joseph on 2/2/20.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT "11299"
+#define MAXDATASIZE 100
+
