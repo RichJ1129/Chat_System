57e1957dfa3c29ec8bdc72b6a8f6c15f86a5f035
Richard Joseph <richjoseph1129@gmail.com>
2020-02-09 04:47:24 -0500

    chat server almost complete.

---
+++ chatserve.py
@@ -0,0 +1,88 @@
+import socket
+import sys
+import os
+
+
+def check_startup():
+    if len(sys.argv) < 2:
+        print("Format of input should be:")
+        print("python3 chatserve.py <port>")
+
+
+def choose_handle():
+    handle = input("What would you like your username to be (Limit 11 Characters):")
+
+    while len(handle) > 11:
+        handle = input("Please choose a handle that is less than 11 characters:")
+
+    return handle
+
+
+def start_server():
+    port_number = int(sys.argv[1])
+    user_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    user_socket.bind(("", port_number))
+    user_socket.listen(1)
+    return user_socket
+
+
+def receive_msg(msg):
+    return msg.recv(256)
+
+
+def send_msg(msg):
+    user_msg = input("User1: ")
+    msg.send("User1: " + user_msg)
+    return user_msg
+
+
+def main():
+    check_startup()
+    new_server = start_server()
+
+    online = True
+
+    while 1:
+        os.system('clear')
+        print("Awaiting message from the client")
+        waiting = True
+        conn, addr = new_server.accept()
+        while 1:
+            received = receive_msg(conn)
+            if "\quit" in received:
+                break
+            if waiting:
+                os.system('clear')
+                print("-Messages-")
+            waiting = False
+            print(received)
+            sent = send_msg(conn)
+            if "\quit" in sent:
+                break
+    conn.close()
+
+
+    # TCP_IP = 'localhost'
+    # TCP_PORT = 11299
+    # BUFFER_SIZE = 256  # Normally 1024, but we want fast response
+    #
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.bind((TCP_IP, TCP_PORT))
+    # s.listen(1)
+    #
+    # conn, addr = s.accept()
+    # print('Connection address:', addr)
+    # while 1:
+    #     data = conn.recv(BUFFER_SIZE)
+    #     if not data: break
+    #     print("received data:", data)
+    #     conn.send(data)  # echo
+    # conn.close()
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
