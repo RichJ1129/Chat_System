51c31d796bcb80d0b6cae39b6d86508657f5456e
Richard Joseph <richjoseph1129@gmail.com>
2020-02-09 05:39:30 -0500

    Updated

--- chatserve.py
+++ chatserve.py
@@ -65,22 +65,5 @@ def main():
     conn.close()
 
 
-    # TCP_IP = 'localhost'
-    # TCP_PORT = 11299
-    # BUFFER_SIZE = 256  # Normally 1024, but we want fast response
-    #
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # s.bind((TCP_IP, TCP_PORT))
-    # s.listen(1)
-    #
-    # conn, addr = s.accept()
-    # print('Connection address:', addr)
-    # while 1:
-    #     data = conn.recv(BUFFER_SIZE)
-    #     if not data: break
-    #     print("received data:", data)
-    #     conn.send(data)  # echo
-    # conn.close()
-
 if __name__ == '__main__':
     main()
